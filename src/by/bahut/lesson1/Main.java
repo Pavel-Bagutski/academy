11d304ecbbe0ff105e11e4f1e1309cd629dc801d
Pavel-Bagutski <pavelbagutski@gmail.com>
2022-11-24 20:36:26 +0300

    first c

---
+++ src/by/bahut/lesson1/Main.java
@@ -0,0 +1,22 @@
+package by.bahut.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = 2123000;
+        int b = 100;
+        int c = 234;
+
+        int sum = a / b * c;
+
+      //  System.out.println(sum);
+
+    int _a = 3*3+4*4;
+        System.out.println(_a);
+    }
+
+
+
+}
+
